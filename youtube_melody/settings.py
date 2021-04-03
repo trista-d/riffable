96d6c2f93f72d635cf3b589787127789072671d9
Trista D <74997356+trista-d@users.noreply.github.com>
2021-04-03 13:36:44 -0700

    cors error

--- youtube_melody/settings.py
+++ youtube_melody/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django_samesite_none.middleware.SameSiteNoneMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
